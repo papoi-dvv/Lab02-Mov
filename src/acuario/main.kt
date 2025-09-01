28fcff5632335f176a669ffa776a04b4aed5513b
papoi-dvv <paulo.santos@tecsup.edu.pe>
2025-08-31 23:00:56 -0500

    Acuario: Herencia Terminado

---
+++ src/acuario/main.kt
@@ -0,0 +1,33 @@
+package acuario
+
+fun construirAcuario() {
+    val acuario1 = Acuario()
+    acuario1.imprimirTamano()
+
+    val acuario2 = Acuario(ancho = 25)
+    acuario2.imprimirTamano()
+
+    val acuario3 = Acuario(alto = 35, largo = 110)
+    acuario3.imprimirTamano()
+
+    val acuario4 = Acuario(ancho = 25, alto = 35, largo = 110)
+    acuario4.imprimirTamano()
+
+    val acuario6 = Acuario(numeroDePeces = 29)
+    acuario6.imprimirTamano()
+
+    val acuario7 = Acuario(numeroDePeces = 29)
+    acuario7.imprimirTamano()
+    acuario7.volumen = 70
+    acuario7.imprimirTamano()
+
+    val acuario8 = Acuario(largo = 25, ancho = 25, alto = 40)
+    acuario8.imprimirTamano()
+
+    val miTorre = TanqueTorre(diametro = 25, alto = 40)
+    miTorre.imprimirTamano()
+}
+
+fun main() {
+    construirAcuario()
+}
